5a2352e9c1ec3c9a32a119a1826e16b5f45968ec
DavidCalalang <dcalalan@purdue.edu>
2024-05-24 22:54:24 +0800

    starting streamlit

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Installing Dependencies
 import os
 from environ import apikey
+import streamlit as st
+from langchain.llms import OpenAI
 
+os.environ['OPENAI_API_KEY'] = apikey
+
+# UI setup
+st.title('Basic Chatbot')
+prompt = st.text_input('Enter Prompt')
